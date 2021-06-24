9e89fa9944f2ea6123f252f42726da779fd66bb3
Tushar Ismail <gwalchmei@gmail.com>
2021-06-24 13:21:13 +0800

    delete contact from UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -42,12 +42,14 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
-const addContact = contact => {
-  contact.id = uuidv4()
-  dispatch({ type: ADD_CONTACT, payload: contact })
-}
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
   // Delete Contact
-
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
   // Set Current Contact
 
   // Clear Current Contact
@@ -62,7 +64,8 @@ const addContact = contact => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact,
       }}
     >
       {props.children}
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -9,13 +9,20 @@ import {
 } from '../types';
 
 export default (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload]
-      }
+        contacts: [...state.contacts, action.payload],
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
-}
+};
